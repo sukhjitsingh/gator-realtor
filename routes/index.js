68cce6a93b1942b7069d6866fe8e9c501a5e65d1
Ilya Nemtsov <nemtsovilya@yahoo.com>
2017-10-12 19:27:35 -0700

    fixed rendering bug

--- routes/index.js
+++ routes/index.js
@@ -6,15 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index.hbs', { title: 'Express' });
 });
 
-
-// Register
-router.get('/register', function(req, res){
-    res.render('register.hbs');
-});
-
-// Login
-router.get('/login', function(req, res){
-    res.render('login.hbs');
-});
-
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// Register
+router.get('/register', function(req, res){
+    res.render('register.hbs');
 });
 
+// Login
+router.get('/login', function(req, res){
+    res.render('login.hbs');
+});
 
 module.exports = router;
