0cde2e824ee6de91f3832775132ea11f673f5d01
Ilya Nemtsov <nemtsovilya@yahoo.com>
2017-10-12 18:01:41 -0700

    added log page

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/users.hbs', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -40,7 +40,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error.hbs');
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index.hbs', { title: 'Express' });
+});
+
+
+// Register
+router.get('/register', function(req, res){
+    res.render('register.hbs');
+});
+
+// Login
+router.get('/login', function(req, res){
+    res.render('login.hbs');
 });
 
 module.exports = router;
